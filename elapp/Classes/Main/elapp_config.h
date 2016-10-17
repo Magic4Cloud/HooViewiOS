40a25e8c460c0e30ef86cafbb8725f9d89f86ddd
ivy_lover <913385445@qq.com>
2016-10-17 14:51:45 +0800

    fix black screen when living

--- elapp/Classes/Main/elapp_config.h
+++ elapp/Classes/Main/elapp_config.h
@@ -9,46 +9,46 @@
 #define elapp_config_h
 
 ///////////////////////////////////// 服务器地址 //////////////////////////////////////
-#error FIXME: Need to change to yours
-#define CCURL_RELEASE                       @"http://"
-#define CCURL_HTTPS_RELEASE                 @"http://"
+//#error FIXME: Need to change to yours
+#define CCURL_RELEASE                       @"http://hooview.evapp.easyvaas.com/easyvaas/appgw/"
+#define CCURL_HTTPS_RELEASE                 @"http://hooview.evapp.easyvaas.com/easyvaas/appgw/"
 #define CCURL_STATTE_RELEASE                @"release"
 
-#define CCURL_DEV                           @"https://"
-#define CCURL_HTTPS_DEV                     @"https://"
+#define CCURL_DEV                           @"http://hooview.evapp.easyvaas.com/easyvaas/appgw/"
+#define CCURL_HTTPS_DEV                     @"http://hooview.evapp.easyvaas.com/easyvaas/appgw/"
 #define CCURL_STATTE_DEV                    @"dev"
 /////////////////////////////////////////////////////////////////////////////////////
 
 ///////////////////////////////////// 第三方key //////////////////////////////////////
 //----------------------------------- 极光推送 ----------------------------------//
-#error FIXME: Need to change to yours
-#define J_PUSH_APP_KEY                      @""
+//#error FIXME: Need to change to yours
+#define J_PUSH_APP_KEY                      @"5058b51ad39a89832f8a65dd"
 #define J_PUSH_APP_CHANNEL                  @"App Store"
 //----------------------------------- 环信推送 ----------------------------------//
-#error FIXME: Need to change to yours
-#define EASEMOBAPP_KEY_RELEASE              @""
-#define EASEMOBAPP_KEY_DEV                  @""
-#define EASEMOBAPP_CER_RELEASE              @""
-#define EASEMOBAPP_CER_DEV                  @""
+//#error FIXME: Need to change to yours
+#define EASEMOBAPP_KEY_RELEASE              @"1150160929178497#hooview"
+#define EASEMOBAPP_KEY_DEV                  @"1150160929178497#hooview"
+#define EASEMOBAPP_CER_RELEASE              @"1150160929178497#hooview"
+#define EASEMOBAPP_CER_DEV                  @"1150160929178497#hooview"
 //----------------------------------- BUGLY崩溃上报 -----------------------------//
-#error FIXME: Need to change to yours
-#define BUGLY_APP_ID                        @""
+//#error FIXME: Need to change to yours
+#define BUGLY_APP_ID                        @"1105755574"
 //----------------------------------- 友盟数据统计 -------------------------------//
-#error FIXME: Need to change to yours
+//#error FIXME: Need to change to yours
 #define UMENG_APP_KEY                       @""
 //----------------------------------- 阿里百川反馈 -------------------------------//
-#error FIXME: Need to change to yours
-#define ALI_FEEDBACK_APP_KEY                @""
+//#error FIXME: Need to change to yours
+#define ALI_FEEDBACK_APP_KEY                @"23471999"
 //----------------------------------- 微博 -------------------------------------//
-#error FIXME: Need to change to yours
-#define WEIBO_APP_KEY                       @""
+//#error FIXME: Need to change to yours
+#define WEIBO_APP_KEY                       @"3385195966"
 //----------------------------------- QQ --------------------------------------//
-#error FIXME: Need to change to yours
-#define QQ_APP_ID                           @""
+//#error FIXME: Need to change to yours
+#define QQ_APP_ID                           @"1105755574"
 //----------------------------------- 微信 -------------------------------------//
-#error FIXME: Need to change to yours
-#define WEIXIN_APP_KEY                      @""
-#define WEIXIN_SECRET_KEY                   @""
+//#error FIXME: Need to change to yours
+#define WEIXIN_APP_KEY                      @"wx89d62b0eec9c1d9c"
+#define WEIXIN_SECRET_KEY                   @"ffb4c8f18145bbf1d19ddd99bef72749"
 //----------------------------------- 易视云服务 --------------------------------//
 
 /////////////////////////////////////////////////////////////////////////////////////
@@ -69,18 +69,18 @@
 // 易视云服务 开发线上区分
 #ifdef STATE_RELEASE
 #error FIXME: Need to change to yours
-#define EV_APP_KEY                          @""
-#define EV_ACCESS_KEY                       @""
+#define EV_APP_KEY                          @"r7Mun6JpcwHiSdIh"
+#define EV_ACCESS_KEY                       @"hdxLQK1jMMRRQlST"
 #endif
 
 #ifdef STATE_DEV
-#error FIXME: Need to change to yours
-#define EV_APP_KEY                          @""
-#define EV_ACCESS_KEY                       @""
+//#error FIXME: Need to change to yours
+#define EV_APP_KEY                          @"r7Mun6JpcwHiSdIh"
+#define EV_ACCESS_KEY                       @"hdxLQK1jMMRRQlST"
 #endif
 
-#error FIXME: Need to change to yours
-#define EV_SECRET_KEY                       @""
+//#error FIXME: Need to change to yours
+#define EV_SECRET_KEY                       @"S6HQDY9M4LKnaosPrR7CZWQnsEvFgHWy"
 
 // 开启日志全局开关e
 #ifdef STATE_DEV
