f7a2c71977c65e6c263a1988037c37e813af7be3
tc1993 <873520982@qq.com>
2017-04-19 16:53:14 +0800

    同步

---
+++ elapp/Classes/elapp/UserPerson/Model/EVVideoAndLiveModel.m
@@ -0,0 +1,13 @@
+//
+//  EVVideoAndLiveModel.m
+//  elapp
+//
+//  Created by 唐超 on 4/19/17.
+//  Copyright © 2017 easyvaas. All rights reserved.
+//
+
+#import "EVVideoAndLiveModel.h"
+
+@implementation EVVideoAndLiveModel
+
+@end
