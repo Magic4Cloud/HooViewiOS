5d7f4858500759b68e665f01645fe77994661635
Zhouheng99 <767092858@qq.com>
2017-05-02 18:32:28 +0800

    优化

--- elapp/Classes/Common/EVNetwork/Constant/constants.h
+++ elapp/Classes/Common/EVNetwork/Constant/constants.h
@@ -114,7 +114,6 @@
 
 
 
-
 #define EVSearchVideoAPI            @"https://dev.yizhibo.tv/hooview/appgw/video/search"
 
 #define EVGoodVideoListAPI          @"/video/vodlist"
@@ -209,9 +208,6 @@
 //个人主页评论列表点赞
 #define EVHVCommentLikeOrNotAPI     @"http://dev.hooview.com/api/v2/posts/like"
 
-//添加收藏和取消收藏
-#define EVCollectAPI                @"http://dev.hooview.com/api/v2/news/favorite"
-
 #endif
 
 
@@ -272,8 +268,8 @@
 #define EVCleanHistoryListAPI       @"http://dev.hooview.com/api/v2/user/clean"
 //我的购买
 #define EVMyShopsAPI                @"http://192.168.8.125:8888/user/purchases"
-//添加收藏和取消收藏
-#define EVCollectAPI                @"http://dev.hooview.com/api/v2/news/favorite"
+
+
 
 #endif
 #endif
